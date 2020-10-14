818bac9c785db9460c99629e3644fedf63909237
PRITHVI2707 <72848299+PRITHVI2707@users.noreply.github.com>
2020-10-14 09:53:18 +0530

    Create message.java

---
+++ message.java
@@ -0,0 +1,31 @@
+package com.example.database;
+
+public class message {
+    String name;
+    String message;
+
+    message() {
+
+    }
+
+    public message(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
